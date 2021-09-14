3ce788e20f858792a1d1a5a4abc8fb351d676924
AsiF-Py <asif.mannanuddin@gmail.com>
2021-09-14 18:12:55 +0600

    change me

--- app/views.py
+++ app/views.py
@@ -115,17 +115,5 @@ def contact(request):
 def about(request):
 	return render(request,'about.html')
 	
-# def contact(request):
-# 	if request.method == 'GET':
-# 		form = ContactForm()
-# 	else:
-# 		form = ContactForm(request.POST)
-# 		if form.is_valid():
-# 			subject = form.cleaned_data['subject']
-# 			from_email = form.cleaned_data['from_email']
-# 			message = form.cleaned_data['message']
-# 			try:
-# 				send_mail(subject, message, from_email, ['asifuddin1152@gmail.com'],fail_silently=False)
-# 			except BadHeaderError:
-# 				return HttpResponse('Invalid header found.')
-# 	return render(request,'contact.html',{'form':form})        
+def home(request):
+	return HttpResponse("hello")
